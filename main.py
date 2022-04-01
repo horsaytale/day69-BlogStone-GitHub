5339d8362cc4b1c81dcba693eb9d3ea4ba04573c
horsaytale <98501452+horsaytale@users.noreply.github.com>
2022-04-01 11:48:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from flask import abort
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = ""
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
